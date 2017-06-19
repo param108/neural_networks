565663a4b4daac19d833c5fae34ec769105b69b3
Param <param@wobe.io>
2017-06-19 11:41:37 +0530

    modes, erase, save, character saving.

--- curses_drawer/README.md
+++ curses_drawer/README.md
@@ -2,12 +2,16 @@
 `make draw`
 
 ## To run
-`./draw`
+`./draw <letter that you are saving>`
 
 ## To use
 - left click to place a mark
 - right click to remove the mark
-- 's' to save
-- 'q' to quit
+- 's' to save and start new
+- 'q' to quit (does not save)
+- 'e' to erase current marks and start again.
+- 'm' toggle mode
+		* CONTINUOUS - left click to start drawing, move mouse to draw, left click again to stop. Similarly for right click and erase.
+		* CLICK - left click to make one mark. right click to erase one mark.
 
-The data will be written as a json array `[[x,y],[x1,y1]....]` (where x,y are coordinates of the marked points) to a file `saved.json`
+The data will be written as a json array `[[[x,y],[x1,y1]....],"<letter>"]` (where x,y are coordinates of the marked points) to a file `saved.json`. If the file already exists it will be appended.
--- curses_drawer/main.c
+++ curses_drawer/main.c
@@ -9,6 +9,7 @@ typedef struct _Pt{
 } Pt;
 
 
+char characterToSave;
 Pt* Head = NULL;
 void allocate(int x, int y) {
 	if (Head == NULL) {
@@ -77,7 +78,7 @@ void save() {
 	FILE *fp;
 	fp = fopen("saved.json","a");
 	Pt* iter = Head;
-	fprintf(fp,"[");
+	fprintf(fp,"[[");
 	while(iter) {
 		fprintf(fp,"[%d,%d]",iter->x, iter->y);
 		if (iter->next != NULL) {
@@ -85,13 +86,32 @@ void save() {
 		}
 		iter = iter->next;
 	}
-	fprintf(fp,"]\n");
+	fprintf(fp,"],\"%c\"],\n",characterToSave);
 	fclose(fp);
 }
 
 
+void draw_screen(WINDOW *win, int mode) {
+	int i,j;
+
+	for (i = 0; i < 50; i++) {
+		mvwaddch(win, i, 50, ACS_VLINE);
+		mvwaddch(win, 50, i, ACS_HLINE);
+	}
+	mvwaddch(win, 50,50, ACS_LRCORNER);
+	if (mode == 0) {
+		mvwaddstr(win, 0, 51,"MODE: CONTINUOUS");
+	} else {
+		mvwaddstr(win, 0, 51,"MODE: CLICK     ");
+	}
+	wrefresh(win);
+}
+
 void main_loop(WINDOW *win) {
 	int c, choice = 0, val;
+	int movement_started = 0;
+	int delete_started = 0;
+	int mode = 0;
 	FILE *fp;
 	fp = fopen("log","w");
 	if (fp == NULL) {
@@ -108,15 +128,49 @@ void main_loop(WINDOW *win) {
 			{	/* When the user clicks left mouse button */
 				fprintf(fp,"Got mouse event\n");
 				fprintf(fp,"%#lx,%#lx\n",event.bstate, BUTTON1_CLICKED);
+				if (event.x >= 50 || event.y >= 50) {
+					break;
+				}
 				if(event.bstate & BUTTON1_CLICKED)
 				{	
 					allocate(event.x, event.y);
 					mvaddch(event.y, event.x,ACS_BLOCK);
-					fprintf(fp,"%d,%d\n",event.y, event.x);
-					refresh(); 
+					if (mode == 0) {
+						if (movement_started) {
+							movement_started = 0;
+						} else {
+							movement_started = 1;
+							delete_started = 0;
+						}
+					}
+					refresh();
 				} else if (event.bstate & BUTTON3_CLICKED) {
 					deallocate(event.x,event.y);
 					mvaddch(event.y, event.x, ' '); 
+					if (mode == 0) {
+						if (delete_started) {
+							delete_started = 0;
+						} else {
+							delete_started = 1;
+							movement_started = 0;
+						}
+					}
+
+					refresh();
+				} 
+				if (event.bstate & REPORT_MOUSE_POSITION) {
+					if (mode != 0) {
+						break;
+					}
+					if (movement_started) {
+						allocate(event.x, event.y);
+						mvaddch(event.y, event.x,ACS_BLOCK);
+						refresh();
+					} else if (delete_started) {
+						deallocate(event.x,event.y);
+						mvaddch(event.y, event.x, ' '); 
+						refresh();
+					}	
 				}
 			}
 			break;
@@ -124,24 +178,59 @@ void main_loop(WINDOW *win) {
 				delete_all();
 				return;
 			break;
+			case 'e':
+				delete_all();
+				erase();
+				draw_screen(win, mode);
+				movement_started = 0;
+				delete_started = 0;
+
+				break;
 			case 's':
 				save();
 				delete_all();
 				erase();
+				draw_screen(win, mode);
+				movement_started = 0;
+				delete_started = 0;
 				break;
+			case 'm':
+				if (mode == 0) {
+					mode = 1;
+					movement_started = 0;
+					delete_started = 0;
+					mvwaddstr(win, 0, 51,"MODE: CLICK     ");
+				} else {
+					mode = 0;
+					mvwaddstr(win, 0, 51,"MODE: CONTINUOUS");
+				}
+				break;
+			default:
+				fprintf(fp,"Got event %d\n",c);
+				break;	
 		}
 		fflush(fp);
 	}
 }
 
-void main() {
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("%s <character>\n",argv[0]);
+		return 1;
+	}
+
+	characterToSave = argv[1][0];
 	initscr();
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE);
 	refresh();
-	mousemask(BUTTON1_CLICKED | BUTTON3_CLICKED, NULL);
+	mousemask(BUTTON1_CLICKED | BUTTON3_CLICKED | REPORT_MOUSE_POSITION, NULL);
 
+	// makes the terminal report mouse movements.
+	printf("\033[?1003h\n"); 
+	draw_screen(stdscr, 0);
 	main_loop(stdscr);
 	endwin();
+	return 0;
 }
